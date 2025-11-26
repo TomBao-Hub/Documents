85b3aeade0252e28a11fe848870d70c04f639cd2
Bao <thienbaobro15@gmail.com>
2025-11-26 15:19:36 +0700

    add

--- Du-an-01/python/main.py
+++ Du-an-01/python/main.py
@@ -1,13 +1,21 @@
-import pygame
+import random
 
-pygame.init()
+random.seed(0)
 
-screen = pygame.display.set_mode((400, 300))
+map_list = [[0 for _ in range(50)] for _ in range(20)]
 
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+def noise():
+    global map_list
+    map_list[0][0] = random.randint(0,1)
+    for i in range(49):
+        map_list[0][1+i] = 
 
-pygame.quit
+
+
+noise()
+draw = '.#'
+for i in range(20):
+    text = ''
+    for j in range(50):
+        text += draw[ map_list[i][j] ]
+    print(text)
